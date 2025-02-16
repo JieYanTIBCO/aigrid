c9897b564a213a4400d6d0aff6382e820aad5520
JieYanTIBCO <kiki3890528@gmail.com>
2025-02-16 11:43:31 -0500

    Before remove src/configs/vite/vite.newtab.config.ts

---
+++ src/configs/vite/vite.newtab.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+    },
+  },
+  build: {
+    emptyOutDir: false,
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        newtab: path.resolve(process.cwd(), 'src/extension/newtab/index.tsx')
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
+})
